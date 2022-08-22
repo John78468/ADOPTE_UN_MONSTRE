b1b41c4a0b13c99db6efc434adcc6fca0377a0d6
Thibault <106171075+thibault-tripoli@users.noreply.github.com>
2022-08-22 17:38:59 +0200

    update monster

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,7 +1,6 @@
 class Monster < ApplicationRecord
   belongs_to :user
   has_many :contracts
-  has_many :users, trough: :contracts
 
   validates :name, :specie, :description, :image, :price, presence: true
 end
