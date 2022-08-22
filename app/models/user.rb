3f950008c316dee9c1687730832bee4ed44ceb9f
Thibault <106171075+thibault-tripoli@users.noreply.github.com>
2022-08-22 17:32:11 +0200

    update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :monsters
+  has_many :monsters, through: :contracts
 
   validates :name, :email, :password, presence: true
 end
