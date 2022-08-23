97abe914a7e95d9480f8b27dbfdd1ea059181912
cardon <philantrope1@outlook.com>
2022-08-23 10:41:26 +0200

    test controller monster

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -7,20 +7,39 @@ class MonstersController < ApplicationController
     @monster = Monster.new
   end
 
+  def create
+    @monster = Monster.new
+    if @monster.save
+      redirect_to monster_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @monster = Monster.find(params[:id])
   end
 
   def edit
+    @monster = Monster.find(params[:id])
   end
 
   def update
-  end
-
-  def create
+    @monster = Monster.find(monster_params)
+    if @monster.update
+      redirecto_to monster_path
+    else
+      render :update, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @monster = Monster.find(monster_params)
+    if @monster.destroy
+      redirecto_to monsters_path
+    else
+      render :destroy, status: :unprocessable_entity
+    end
   end
 
   private
