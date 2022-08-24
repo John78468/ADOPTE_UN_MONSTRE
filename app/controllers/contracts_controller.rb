f144fb57f6843f26938f5d53a10712a502f2a310
Ilyes Soumar Djouma <isoumardjouma@reseau.free.fr>
2022-08-24 13:46:13 +0200

    show_contracts

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,22 +1,23 @@
 class ContractsController < ApplicationController
 
   def new
-    @actif = true
-    @battles = 0
     @monster = Monster.find(params[:monster_id])
     @contract = Contract.new
     @contract.monster = @monster
     @contract.user = current_user
+    @contract.battles = 0
   end
 
   def create
     @contract = Contract.new(contract_params)
     @contract.user = current_user
     @contract.monster = Monster.find(params[:monster_id])
+    @contract.actif = true
     if @contract.save
       redirect_to profil_path(current_user)
     else
       render :new, status: :unprocessable_entity
+      # redirect_to profil_path(current_user)
     end
   end
 
