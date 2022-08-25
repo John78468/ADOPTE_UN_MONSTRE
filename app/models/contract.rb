ff65a30b8bdf0061610f30c89fd76e5a57a970fb
cardon <philantrope1@outlook.com>
2022-08-25 16:59:10 +0200

    insert profil coinPogs, nb monsters, nb contrat

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,5 +2,5 @@ class Contract < ApplicationRecord
   belongs_to :monster
   belongs_to :user
   # validates_uniqueness_of :monster_id, conditions: -> { where(actif: true) }
-  validates :battles, presence: true,  numericality: { only_integer: true }
+  validates :battles, presence: true, numericality: { only_integer: true }
 end
